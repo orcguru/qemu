4cde55c61226d5097f4e0fa1f471f66239749ef2
Felix Wang <wangting@gmail.com>
2025-09-03 12:55:39 +0800

    add alloca/store for xreg/slots

--- ir-translator/tcg_ast.c
+++ ir-translator/tcg_ast.c
@@ -26,6 +26,8 @@ const char *fixed_arg_names[FIXED_PARAM_COUNT] = {NULL};
 LLVMTypeRef fixed_vector_param_types[FIXED_VECTOR_PARAM_COUNT] = {NULL};
 const char *fixed_vector_arg_names[FIXED_VECTOR_PARAM_COUNT] = {NULL};
 const char *fixed_vector_stack_names[FIXED_VECTOR_PARAM_COUNT] = {NULL};
+const char *tmpl_stack_names[1<<5] = {NULL};
+const char *tmpt_stack_names[1<<5] = {NULL};
 
 static LLVMModuleRef create_module(const char *module_name) {
     LLVMContextRef context = LLVMGetGlobalContext();
@@ -39,11 +41,15 @@ void handle_func(uint64_t val) {
     void *ptr_init = get_instr_buffer();
     void *ptr_max = ptr_init + get_instr_buffer_size();
     void *ptr;
+    int tmpl_dirty = 0, tmpt_dirty = 0;
     uint32_t xreg_valid = 0, tmpl_valid = 0, tmpt_valid = 0, is_imm = 0;
     uint32_t tmpl_128bit = 0, tmpl_64bit = 0, tmpl_32bit = 0, tmpl_16bit = 0, tmpl_8bit = 0;
     uint32_t tmpt_128bit = 0, tmpt_64bit = 0, tmpt_32bit = 0, tmpt_16bit = 0, tmpt_8bit = 0;
+    uint8_t tmpl_bits_type[1<<5];
+    uint8_t tmpt_bits_type[1<<5];
     uint64_t xmm_valid = 0;
-    for (ptr = ptr_init; ptr < ptr_max; ptr = move_to_next(ptr)) {
+    int instr_idx = 0;
+    for (ptr = ptr_init; ptr < ptr_max; ptr = move_to_next(ptr), instr_idx += 1) {
         uint32_t slot_idx = 0;
         OperandType operand;
         OpCodeType opc = get_opcode(ptr);
@@ -56,57 +62,61 @@ void handle_func(uint64_t val) {
                 if (operand.s.slot_type == SUB_SLOT_XREG) {
                     xreg_valid |= (1 << operand.s.slot_idx);
                 } else if (operand.s.slot_type == SUB_SLOT_TMPL) {
-                    tmpl_valid |= (1 << operand.s.slot_idx);
+                    tmpl_dirty = 1;
+                    uint32_t slot_bit = (1 << operand.s.slot_idx);
+                    tmpl_valid |= slot_bit;
                     if (slot_idx < opcoc[opc]) {
                         if (opciosz[opc][2] == 64) {
-                            tmpl_64bit |= (1 << operand.s.slot_idx);
+                            tmpl_64bit |= slot_bit;
                         } else if (opciosz[opc][2] == 32) {
-                            tmpl_32bit |= (1 << operand.s.slot_idx);
+                            tmpl_32bit |= slot_bit;
                         } else if (opciosz[opc][2] == 16) {
-                            tmpl_16bit |= (1 << operand.s.slot_idx);
+                            tmpl_16bit |= slot_bit;
                         } else if (opciosz[opc][2] == 8) {
-                            tmpl_8bit |= (1 << operand.s.slot_idx);
+                            tmpl_8bit |= slot_bit;
                         } else if (opciosz[opc][2] == 128) {
-                            tmpl_128bit |= (1 << operand.s.slot_idx);
+                            tmpl_128bit |= slot_bit;
                         }
                     } else {
                         if (opciosz[opc][0] == 64) {
-                            tmpl_64bit |= (1 << operand.s.slot_idx);
+                            tmpl_64bit |= slot_bit;
                         } else if (opciosz[opc][0] == 32) {
-                            tmpl_32bit |= (1 << operand.s.slot_idx);
+                            tmpl_32bit |= slot_bit;
                         } else if (opciosz[opc][0] == 16) {
-                            tmpl_16bit |= (1 << operand.s.slot_idx);
+                            tmpl_16bit |= slot_bit;
                         } else if (opciosz[opc][0] == 8) {
-                            tmpl_8bit |= (1 << operand.s.slot_idx);
+                            tmpl_8bit |= slot_bit;
                         } else if (opciosz[opc][0] == 128) {
-                            tmpl_128bit |= (1 << operand.s.slot_idx);
+                            tmpl_128bit |= slot_bit;
                         }
                     }
                 } else if (operand.s.slot_type == SUB_SLOT_TMPT) {
-                    tmpt_valid |= (1 << operand.s.slot_idx);
+                    tmpt_dirty = 1;
+                    uint32_t slot_bit = (1 << operand.s.slot_idx);
+                    tmpt_valid |= slot_bit;
                     if (slot_idx < opcoc[opc]) {
                         if (opciosz[opc][2] == 64) {
-                            tmpt_64bit |= (1 << operand.s.slot_idx);
+                            tmpt_64bit |= slot_bit;
                         } else if (opciosz[opc][2] == 32) {
-                            tmpt_32bit |= (1 << operand.s.slot_idx);
+                            tmpt_32bit |= slot_bit;
                         } else if (opciosz[opc][2] == 16) {
-                            tmpt_16bit |= (1 << operand.s.slot_idx);
+                            tmpt_16bit |= slot_bit;
                         } else if (opciosz[opc][2] == 8) {
-                            tmpt_8bit |= (1 << operand.s.slot_idx);
+                            tmpt_8bit |= slot_bit;
                         } else if (opciosz[opc][2] == 128) {
-                            tmpt_128bit |= (1 << operand.s.slot_idx);
+                            tmpt_128bit |= slot_bit;
                         }
                     } else {
                         if (opciosz[opc][0] == 64) {
-                            tmpt_64bit |= (1 << operand.s.slot_idx);
+                            tmpt_64bit |= slot_bit;
                         } else if (opciosz[opc][0] == 32) {
-                            tmpt_32bit |= (1 << operand.s.slot_idx);
+                            tmpt_32bit |= slot_bit;
                         } else if (opciosz[opc][0] == 16) {
-                            tmpt_16bit |= (1 << operand.s.slot_idx);
+                            tmpt_16bit |= slot_bit;
                         } else if (opciosz[opc][0] == 8) {
-                            tmpt_8bit |= (1 << operand.s.slot_idx);
+                            tmpt_8bit |= slot_bit;
                         } else if (opciosz[opc][0] == 128) {
-                            tmpt_128bit |= (1 << operand.s.slot_idx);
+                            tmpt_128bit |= slot_bit;
                         }
                     }
                 } else if (operand.s.slot_type == SUB_SLOT_XMM) {
@@ -115,6 +125,42 @@ void handle_func(uint64_t val) {
             }
             slot_idx += 1;
         } while (1);
+    }
+    if (tmpl_dirty) {
+        for (int i = 0; i < (1<<5); ++i) {
+            uint32_t slot_bit = (1 << i);
+            if (tmpl_valid & slot_bit) {
+                if (tmpl_128bit & slot_bit) {
+                    tmpl_bits_type[i] = 4;
+                } else if (tmpl_64bit & slot_bit) {
+                    tmpl_bits_type[i] = 3;
+                } else if (tmpl_32bit & slot_bit) {
+                    tmpl_bits_type[i] = 2;
+                } else if (tmpl_16bit & slot_bit) {
+                    tmpl_bits_type[i] = 1;
+                } else if (tmpl_8bit & slot_bit) {
+                    tmpl_bits_type[i] = 0;
+                }
+            }
+        }
+    }
+    if (tmpt_dirty) {
+        for (int i = 0; i < (1<<5); ++i) {
+            uint32_t slot_bit = (1 << i);
+            if (tmpt_valid & slot_bit) {
+                if (tmpt_128bit & slot_bit) {
+                    tmpt_bits_type[i] = 4;
+                } else if (tmpt_64bit & slot_bit) {
+                    tmpt_bits_type[i] = 3;
+                } else if (tmpt_32bit & slot_bit) {
+                    tmpt_bits_type[i] = 2;
+                } else if (tmpt_16bit & slot_bit) {
+                    tmpt_bits_type[i] = 1;
+                } else if (tmpt_8bit & slot_bit) {
+                    tmpt_bits_type[i] = 0;
+                }
+            }
+        }
     }
 
     char func_name[64];
@@ -138,6 +184,56 @@ void handle_func(uint64_t val) {
             LLVMValueRef alloca_inst = LLVMBuildAlloca(builder, fixed_vector_param_types[x], fixed_vector_stack_names[x]);
             LLVMSetAlignment(alloca_inst, 8);
             LLVMSetAlignment(LLVMBuildStore(builder, LLVMGetParam(llvm_func, x), alloca_inst), 8);
+        }
+    }
+
+    LLVMTypeRef tmp_types[5] = {NULL, NULL, NULL, NULL, NULL};
+    if (tmpl_dirty) {
+        for (int i = 0; i < (1<<5); ++i) {
+            if (tmpl_valid & (1 << i)) {
+                int type_idx = tmpl_bits_type[i];
+                if (tmp_types[type_idx] == NULL) {
+                    if (type_idx == 0) {
+                        tmp_types[type_idx] = LLVMInt8Type();
+                    } else if (type_idx == 1) {
+                        tmp_types[type_idx] = LLVMInt16Type();
+                    } else if (type_idx == 2) {
+                        tmp_types[type_idx] = LLVMInt32Type();
+                    } else if (type_idx == 3) {
+                        tmp_types[type_idx] = LLVMInt64Type();
+                    } else if (type_idx == 4) {
+                        tmp_types[type_idx] = LLVMVectorType(LLVMInt8Type(), 16);
+                    } else {
+                        assert(0);
+                    }
+                }
+                LLVMValueRef alloca_inst = LLVMBuildAlloca(builder, tmp_types[type_idx], tmpl_stack_names[i]);
+                LLVMSetAlignment(alloca_inst, type_idx <= 3 ? 8 : 16);
+            }
+        }
+    }
+    if (tmpt_dirty) {
+        for (int i = 0; i < (1<<5); ++i) {
+            if (tmpt_valid & (1 << i)) {
+                int type_idx = tmpt_bits_type[i];
+                if (tmp_types[type_idx] == NULL) {
+                    if (type_idx == 0) {
+                        tmp_types[type_idx] = LLVMInt8Type();
+                    } else if (type_idx == 1) {
+                        tmp_types[type_idx] = LLVMInt16Type();
+                    } else if (type_idx == 2) {
+                        tmp_types[type_idx] = LLVMInt32Type();
+                    } else if (type_idx == 3) {
+                        tmp_types[type_idx] = LLVMInt64Type();
+                    } else if (type_idx == 4) {
+                        tmp_types[type_idx] = LLVMVectorType(LLVMInt8Type(), 16);
+                    } else {
+                        assert(0);
+                    }
+                }
+                LLVMValueRef alloca_inst = LLVMBuildAlloca(builder, tmp_types[type_idx], tmpt_stack_names[i]);
+                LLVMSetAlignment(alloca_inst, type_idx <= 3 ? 8 : 16);
+            }
         }
     }
 
@@ -175,6 +271,8 @@ void module_prolog() {
     }
     static char extra_name_buf[30][16];
     static char stack_name_buf[FIXED_VECTOR_PARAM_COUNT][16];
+    static char tmpl_name_buf[1<<5][16];
+    static char tmpt_name_buf[1<<5][16];
     for (int i = 0; i < (FIXED_VECTOR_PARAM_COUNT - FIXED_PARAM_COUNT)/2; ++i) {
         int idx = FIXED_PARAM_COUNT + i * 2;
         fixed_vector_param_types[idx] = vscale_i64;
@@ -187,6 +285,12 @@ void module_prolog() {
     for (int i = 0; i < FIXED_VECTOR_PARAM_COUNT; ++i) {
         snprintf(stack_name_buf[i], sizeof(stack_name_buf[i]), "%s.stack", fixed_vector_arg_names[i]);
         fixed_vector_stack_names[i] = stack_name_buf[i];
+    }
+    for (int i = 0; i < (1<<5); ++i) {
+        snprintf(tmpl_name_buf[i], sizeof(tmpl_name_buf[i]), "loc%d.stack", i);
+        tmpl_stack_names[i] = tmpl_name_buf[i];
+        snprintf(tmpt_name_buf[i], sizeof(tmpt_name_buf[i]), "tmp%d.stack", i);
+        tmpt_stack_names[i] = tmpt_name_buf[i];
     }
 }
 
