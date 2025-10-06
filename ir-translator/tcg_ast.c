d1f6548ff89e6e1f1333de4d831cc804ff9874da
Felix Wang <wangting@gmail.com>
2025-10-06 11:21:18 +0800

    rework translate_call to handle multiple calls with one translation unit

--- ir-translator/tcg_ast.c
+++ ir-translator/tcg_ast.c
@@ -17,8 +17,9 @@
 #include <llvm-c/BitReader.h>
 #include <llvm-c/Linker.h>
 #include <stdbool.h>
+#include <glib.h>
 
-#define DEBUG                       1
+//#define DEBUG                       1
 // FIXME: maybe change all uint8_t to int???
 #define OPC_INPUT_T         opciosz[opc][0]
 #define OPC_EFFECTIVE_T     opciosz[opc][1]
@@ -345,12 +346,17 @@ static uint8_t xmmt_valid[2] = {0};
 static uint32_t xreg_valid = 0, tmp_valid = 0, xmm_valid = 0;
 static LLVMType tmp_bits_type[1<<5] = {0};
 static uint32_t func_instr_cnt_remain = 0;
-static uint8_t current_active_labels[BB_MAX_CNT];
-static uint8_t current_active_label_cnt = 0;
-static uint8_t exception_or_interrupt_on = 0;
 static char output_file[128] = {0};
 #define LLVMNoInlineAttribute       32
 #define LLVMAlwaysInlineAttribute   3
+
+typedef struct active_label_info {
+    LLVMValueRef llvm_func;
+    uint8_t current_active_label_cnt;
+    uint8_t current_active_labels[BB_MAX_CNT];
+    struct active_label_info *next;
+} active_label_info_t;
+static GHashTable *current_active_label_info = NULL;
 
 static void do_store(OpCodeType opc, LLVMValueRef val, LLVMType val_tidx, OperandType out);
 static LLVMValueRef get_env_ptr_raw();
@@ -367,6 +373,10 @@ static int collect_arguments(OpCodeType opc, LLVMValueRef *out_args, int with_fi
 static LLVMValueRef get_or_add_func_with_qemuaot_cc(const char *name, LLVMTypeRef *types, int cnt, int with_ret, LLVMAttributeRef attr_inline_ctrl);
 static void setup_func_stack();
 static LLVMValueRef get_trampoline(LLVMValueRef helper_func, uint8_t do_return, uint8_t with_ret, uint8_t param_cnt, uint8_t env_idx, OperandType *operands, uint32_t *is_imm, LLVMValueRef next_func);
+static void register_labels_for_func(LLVMValueRef func);
+static uint8_t *get_current_active_labels();
+static uint8_t get_current_active_label_cnt(void);
+static void set_current_active_label_cnt(uint8_t current_active_label_cnt);
 
 #define GET_2_OPERANDS()                                \
     do {                                                \
@@ -418,7 +428,7 @@ static LLVMValueRef get_trampoline(LLVMValueRef helper_func, uint8_t do_return, 
     } while (0)
 
 static uint8_t is_opc_end_of_control_flow(OpCodeType opc) {
-    if (opc == jmp_direct || opc == call_direct || opc == ret) {
+    if (opc == jmp_direct || opc == call_direct || opc == call || opc == ret) {
         return 1;
     }
     return 0;
@@ -1505,6 +1515,43 @@ void translate_bswap64_i64(OpCodeType opc, void *ptr) {
     CREATE_OR(operand0, t0, t1);
 }
 
+static uint8_t *get_current_active_labels() {
+    active_label_info_t *info = g_hash_table_lookup(current_active_label_info, llvm_func);
+    assert(info);
+    while (info) {
+        if (info->llvm_func == llvm_func) {
+            return info->current_active_labels;
+        }
+        info = info->next;
+    }
+    assert(0);
+    return NULL;
+}
+
+static uint8_t get_current_active_label_cnt() {
+    active_label_info_t *info = g_hash_table_lookup(current_active_label_info, llvm_func);
+    assert(info);
+    while (info) {
+        if (info->llvm_func == llvm_func) {
+            return info->current_active_label_cnt;
+        }
+        info = info->next;
+    }
+    assert(0);
+    return 0;
+}
+
+static void set_current_active_label_cnt(uint8_t current_active_label_cnt) {
+    active_label_info_t *info = g_hash_table_lookup(current_active_label_info, llvm_func);
+    assert(info);
+    while (info) {
+        if (info->llvm_func == llvm_func) {
+            info->current_active_label_cnt = current_active_label_cnt;
+        }
+        info = info->next;
+    }
+}
+
 void translate_set_label(OpCodeType opc, void *ptr) {
 #ifdef DEBUG
     printf("%s %s %lx\n", __FUNCTION__, opcode_type_str[opc], ptr); fflush(NULL);
@@ -1530,6 +1577,8 @@ void translate_set_label(OpCodeType opc, void *ptr) {
     last_active_bb = label;
 
     int do_move = 0;
+    uint8_t current_active_label_cnt = get_current_active_label_cnt();
+    uint8_t *current_active_labels = get_current_active_labels();
     for (int i = 0; i < current_active_label_cnt; ++i) {
         if (do_move) {
             current_active_labels[i-1] = current_active_labels[i];
@@ -1538,7 +1587,10 @@ void translate_set_label(OpCodeType opc, void *ptr) {
             do_move = 1;
         }
     }
-    current_active_label_cnt -= 1;
+    if (do_move) {
+        current_active_label_cnt -= 1;
+        set_current_active_label_cnt(current_active_label_cnt);
+    }
 }
 
 void translate_brcond_i64(OpCodeType opc, void *ptr) {
@@ -1567,12 +1619,15 @@ void translate_brcond_i64(OpCodeType opc, void *ptr) {
     sprintf(true_bb_name, "bb_L%d", lbl);
     LLVMBasicBlockRef bb_true = get_bb(true_bb_name);
     if (!bb_true) {
+        uint8_t current_active_label_cnt = get_current_active_label_cnt();
+        uint8_t *current_active_labels = get_current_active_labels();
         bb_true = LLVMAppendBasicBlock(llvm_func, true_bb_name);
         for (int i = 0; i < current_active_label_cnt; ++i) {
             assert(current_active_labels[i] != lbl);
         }
         current_active_labels[current_active_label_cnt] = lbl;
         current_active_label_cnt += 1;
+        set_current_active_label_cnt(current_active_label_cnt);
     }
     LLVMPositionBuilderAtEnd(builder, last_active_bb);
     LLVMBuildCondBr(builder, bool_val, bb_true, bb_false);
@@ -1933,6 +1988,9 @@ static int collect_arguments(OpCodeType opc, LLVMValueRef *out_args, int with_fi
 }
 
 static LLVMValueRef get_or_add_func_with_qemuaot_cc(const char *name, LLVMTypeRef *types, int cnt, int with_ret, LLVMAttributeRef attr_inline_ctrl) {
+#ifdef DEBUG
+    printf("%s %s\n", __FUNCTION__, name); fflush(NULL);
+#endif
     LLVMValueRef func = LLVMGetNamedFunction(module, name);
     if (!func) {
         LLVMTypeRef func_type = LLVMFunctionType(with_ret ? LLVMInt64Type() : LLVMVoidType(), types, cnt, 0);
@@ -1944,6 +2002,7 @@ static LLVMValueRef get_or_add_func_with_qemuaot_cc(const char *name, LLVMTypeRe
     return func;
 }
 
+// FIXME: update translate_call to be re-entrant safe!
 void translate_call(OpCodeType opc, void *ptr) {
 #ifdef DEBUG
     printf("%s %s %lx\n", __FUNCTION__, opcode_type_str[opc], ptr); fflush(NULL);
@@ -1972,12 +2031,9 @@ void translate_call(OpCodeType opc, void *ptr) {
     }
 
     uint8_t env_idx = get_env_idx(ptr);
-    static int check_recursive_call = 0;
-    assert(check_recursive_call == 0);
-    check_recursive_call = 1;
-
     char second_half_name[64];
     sprintf(second_half_name, "func_%lx_call%d", current_func_offset, current_call_idx);
+    current_call_idx += 1;
     uint8_t noargs = get_helper_noargs(ptr);
     OperandType oarg;
     oarg.s.valid = 0;
@@ -2029,6 +2085,7 @@ void translate_call(OpCodeType opc, void *ptr) {
 
     // Get the second half
     LLVMValueRef second_half_func = get_or_add_func_with_qemuaot_cc(second_half_name, fixed_vector_param_types, FIXED_VECTOR_PARAM_COUNT + noargs, 0, AlwaysInlineAttr);
+    register_labels_for_func(second_half_func);
     LLVMValueRef call_args[FIXED_VECTOR_PARAM_COUNT + MAX_OPERANDS_COUNT] = {NULL};
     int call_arg_cnts = 0;
     uint8_t do_inline_helper = all_alias ? can_inline_helper(h, build_macro, bc_name) : 0;
@@ -2089,11 +2146,15 @@ void translate_call(OpCodeType opc, void *ptr) {
         LLVMBuildRetVoid(builder);
     }
 
+    LLVMValueRef llvm_func_backup = llvm_func;
     // Check if we got remaining BBs
     do {
+        llvm_func = llvm_func_backup;
+        uint8_t current_active_label_cnt = get_current_active_label_cnt();
         if (!current_active_label_cnt) {
             break;
         }
+        uint8_t *current_active_labels = get_current_active_labels();
         uint8_t tgt_lbl = current_active_labels[0];
         void *ptr_init = get_instr_buffer();
         void *ptr_max = ptr_init + get_instr_buffer_size();
@@ -2115,7 +2176,6 @@ void translate_call(OpCodeType opc, void *ptr) {
     } while (1);
 
     if (second_half_disabled) {
-        check_recursive_call = 0;
         return;
     }
 
@@ -2149,7 +2209,6 @@ void translate_call(OpCodeType opc, void *ptr) {
     } else {
         assert(!noargs);
     }
-    check_recursive_call = 0;
 }
 
 static void cleanup_func_resource() {
@@ -2173,8 +2232,6 @@ static void cleanup_func_resource() {
     tmp_valid = 0;
     xmm_valid = 0;
     func_instr_cnt_remain = 0;
-    current_active_label_cnt = 0;
-    exception_or_interrupt_on = 0;
     memset(fixed_vector_param_in_stack, 0, sizeof(fixed_vector_param_in_stack));
     memset(tmp_shadow_offset, 0, sizeof(tmp_shadow_offset));
     memset(tmp_bits_type, 0, sizeof(tmp_bits_type));
@@ -2238,6 +2295,22 @@ static void setup_func_stack() {
                 xmmt_valid[i%2] = 1;
             }
         }
+    }
+}
+
+static void register_labels_for_func(LLVMValueRef func) {
+    active_label_info_t *label_info = (active_label_info_t *)calloc(1, sizeof(active_label_info_t));
+    label_info->llvm_func = func;
+    active_label_info_t *info = g_hash_table_lookup(current_active_label_info, func);
+    if (!info) {
+        g_hash_table_insert(current_active_label_info, func, label_info);
+    } else {
+        while (info->next) {
+            assert(info->llvm_func != func);
+            info = info->next;
+        }
+        assert(info->llvm_func != func);
+        info->next = label_info;
     }
 }
 
@@ -2357,7 +2430,7 @@ void handle_func(uint64_t val) {
         LLVMValueRef param = LLVMGetParam(llvm_func, j);
         LLVMSetValueName(param, fixed_vector_arg_names[j]);
     }
-
+    register_labels_for_func(llvm_func);
     LLVMBasicBlockRef entry = LLVMAppendBasicBlock(llvm_func, "entry");
     LLVMPositionBuilderAtEnd(builder, entry);
     last_active_bb = entry;
@@ -2378,12 +2451,7 @@ void handle_func(uint64_t val) {
     // Handle each IR translation
     for (ptr = ptr_init; ptr < ptr_max; ptr = move_to_next(ptr), func_instr_cnt_remain -= 1) {
         OpCodeType opc = get_opcode(ptr);
-        if (!exception_or_interrupt_on) {
-            handle_single_instr(opc, ptr);
-        }
-        if (opc == call) {
-            current_call_idx += 1;
-        }
+        handle_single_instr(opc, ptr);
         tmp_var_available = tmp_var_available_backup;
     }
 
@@ -2820,6 +2888,9 @@ void module_prolog() {
     llvm_predicate[lt] = LLVMIntSLT;
     llvm_predicate[ltu] = LLVMIntULT;
     llvm_predicate[ne] = LLVMIntNE;
+
+    current_active_label_info = g_hash_table_new(NULL, NULL);
+    assert(current_active_label_info);
 }
 
 void module_epilog() {
