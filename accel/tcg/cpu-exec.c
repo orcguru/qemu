c02e0fc2fb3d0b21c5f9c4fb93242bfceb2320a7
Your Name <you@example.com>
2026-03-06 13:18:59 +0800

    misc

--- accel/tcg/cpu-exec.c
+++ accel/tcg/cpu-exec.c
@@ -266,7 +266,7 @@ extern unsigned long qemu_legacy_log_func;
 static void log_cpu_exec(vaddr pc, CPUState *cpu,
                          const TranslationBlock *tb)
 {
-    if (qemu_log_in_addr_range(pc) && (qemu_legacy_log_func == 0 || ((pc - qemu_legacy_log_func) < 0x100))) {
+    if (qemu_log_in_addr_range(pc) && (qemu_legacy_log_func == 0 || (pc >= qemu_legacy_log_func && (pc - qemu_legacy_log_func) < 0x100))) {
         qemu_log_mask(CPU_LOG_EXEC,
                       "Trace %d: %p [%08" PRIx64
                       "/%016" VADDR_PRIx "/%08x/%08x] %s\n",
