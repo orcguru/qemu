592d3d193aa7c5037723d888e66824562819184e
Your Name <you@example.com>
2026-03-20 09:13:38 +0800

    test AOT_LEVEL_MAX

--- include/tcg/tcg-aot.h
+++ include/tcg/tcg-aot.h
@@ -28,7 +28,7 @@
 #define AOT_LEVEL_0                 0
 #define AOT_LEVEL_1                 1
 #define AOT_LEVEL_MAX               3
-#define AOT_LEVEL                   AOT_LEVEL_1
+#define AOT_LEVEL                   AOT_LEVEL_MAX
 
 typedef struct CodeFragment {
     uint64_t target_addr;
