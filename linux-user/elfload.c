0c92a96b3118aa609ed9daabab11d2bb2f9b5d1b
Felix Wang <wangting@gmail.com>
2026-06-25 17:34:00 +0800

    try to optimize indirect branch by using more space

--- linux-user/elfload.c
+++ linux-user/elfload.c
@@ -3286,13 +3286,10 @@ static bool parse_elf_properties(const ImageSource *src,
     }
 }
 
-#define LINK_API    invoke_lightlink
-//#define LINK_API    invoke_jitlink
-
 #ifdef AOT
 #include "tcg/tcg-aot.h"
 #include <pthread.h>
-extern void *LINK_API(const char *, uint64_t, void (*)(uint64_t, uint64_t, uint64_t), void (*)(const char *, uint64_t), void (*)(const char *), void *, size_t, int, const char *);
+extern void *invoke_lightlink(const char *, uint64_t, void (*)(uint64_t, uint64_t, uint64_t), void (*)(const char *, uint64_t), void (*)(const char *), void *(*)(uint64_t), void *, size_t, int, const char *);
 extern helper_func_t helper_funcs[];
 extern size_t helper_funcs_count;
 extern int enable_llvm_debug;
@@ -3348,8 +3345,8 @@ void load_aot_image(const char *image_name, unsigned long start_code, unsigned l
     }
     char entry_func[128] = {0};
     sprintf(entry_func, "--entry=%s_func_0", tag_start);
-    qemu_log_mask(LOG_AOT, "%s on %s with entry_info:%s\n", "LINK_API", aotnamebuf, entry_func);
-    LINK_API((const char *)aotnamebuf, start_code, tb_aot_insert, tb_aot_log_funcmap, tb_aot_log_message, (void *)helper_funcs, helper_funcs_count, enable_llvm_debug, (const char *)entry_func);
+    qemu_log_mask(LOG_AOT, "%s on %s with entry_info:%s\n", "invoke_lightlink", aotnamebuf, entry_func);
+    invoke_lightlink((const char *)aotnamebuf, start_code, tb_aot_insert, tb_aot_log_funcmap, tb_aot_log_message, g_malloc0, (void *)helper_funcs, helper_funcs_count, enable_llvm_debug, (const char *)entry_func);
 
     aot_range_info_t *info_ptr = g_malloc(sizeof(aot_range_info_t));
     info_ptr->x_addr_range_begin = start_code;
