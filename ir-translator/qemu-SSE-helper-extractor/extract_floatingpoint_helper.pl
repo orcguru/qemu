eb15ba3ff77ff869464714cbad7eae1fbf1e8af7
Felix Wang <wangting@gmail.com>
2026-07-29 14:04:50 +0800

    disable check
    (cherry picked from commit d9fbf5a8abbd58bbbf3074ab3ee05fcc6dbe2132)

--- ir-translator/qemu-SSE-helper-extractor/extract_floatingpoint_helper.pl
+++ ir-translator/qemu-SSE-helper-extractor/extract_floatingpoint_helper.pl
@@ -876,7 +876,7 @@ while (<FD>) {
             }
           }
           if (not ($sym_info[0]->{'SYM'} eq "cc_src" or $sym_info[0]->{'SYM'} eq "cc_dst" or $sym_info[0]->{'SYM'} eq "cc_op" or $sym_info[0]->{'SYM'} eq "regs" or $sym_info[0]->{'SYM'} eq "xmm_regs")) {
-            die "$sym_info[0]->{'SYM'}" if $sym_info[0]->{'IS_ARRAY'} == 1;
+            #die "$sym_info[0]->{'SYM'}" if $sym_info[0]->{'IS_ARRAY'} == 1;
             die "" if $func_ptr->{'ENV_TYPE'} eq "NA";
             $func_ptr->{'DO_DEFINE_ENV'} = 1;
             $func_ptr->{'ENVVAR_AND_VECTORS'}->{"env->$sym_info[0]->{'SYM'}"} = 1;
